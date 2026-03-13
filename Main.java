a0ee885abe754f8b3356452cf8f12c640088206c
champcloud514 <akwasiabubonsrah635@gmail.com>
2026-03-13 20:12:04 +0000

    Java updated

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello from App");
+	System.out.println("This is line 2");
+	System.out.println("Here is line 3");
     }
 }
