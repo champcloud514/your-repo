0317d9dbe0ecfdc4f9481eabdb16c23ed419a4f8
champcloud514 <akwasiabubonsrah635@gmail.com>
2026-03-14 11:02:23 +0000

    jenkinsfile updated witth test stage

---
+++ java-hello-app/src/Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from App");
+        System.out.println("This is line two!");
+        System.out.println("And here’s line three.");
+    }
+
+    // Simple method to test
+    public static String greet(String name) {
+        return "Hello, " + name;
+    }
+}
